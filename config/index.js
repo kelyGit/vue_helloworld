fec8dfe8573161848613c9a66ad08be6f4cb3c1d
rst_wuzhenjie <904002045@qq.com>
2020-04-15 22:27:42 +0800

    访问404

--- config/index.js
+++ config/index.js
@@ -10,11 +10,11 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8089, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
