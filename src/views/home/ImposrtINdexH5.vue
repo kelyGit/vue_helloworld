f3c8a2341f07d55aa25132efe65af17d48eca038
rst_wuzhenjie <wuzhenjie@sz-rst.com>
2020-04-16 18:30:51 +0800

    vue引用h5Index 静态页面

---
+++ src/views/home/ImposrtINdexH5.vue
@@ -0,0 +1,14 @@
+<template >
+  <v-html-panel  src="../../../h5Index/index.html"></v-html-panel>
+</template>
+
+<script>
+  // import  h5Index from '../../../h5Index\index.html';
+  export default {
+    name: 'ImposrtINdexH5'
+  }
+</script>
+
+<style scoped>
+
+</style>
